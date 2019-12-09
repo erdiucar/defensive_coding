74f5f0aa7cf1d93d2f8df16635f26cbea16e5c45
Erdi Uçar <erdi@erdiucar.com>
2019-12-09 17:51:15 +0300

    Add business logic classes.

---
+++ DefensiveCoding.BusinessLogic/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DefensiveCoding.BusinessLogic
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
