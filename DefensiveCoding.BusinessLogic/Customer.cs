571323d6c99b7379da2ddeaf65c45b71fac58792
Erdi Uçar <erdi@erdiucar.com>
2019-12-10 15:35:46 +0300

    Change customer class.

--- DefensiveCoding.BusinessLogic/Customer.cs
+++ DefensiveCoding.BusinessLogic/Customer.cs
@@ -9,6 +9,26 @@ namespace DefensiveCoding.BusinessLogic
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        public decimal CalculatePercentOfGoalSteps(string goalSteps, string actualSteps)
+        {
+            decimal goalStepCount = 0;
+            decimal actualStepCount = 0;
+
+            if (string.IsNullOrWhiteSpace(goalSteps)) throw new ArgumentException(message: "Goal must be entered", paramName: nameof(goalSteps));
+            if (string.IsNullOrWhiteSpace(actualSteps)) throw new ArgumentException(message: "Actual steps count must be entered", paramName: nameof(actualSteps));
+
+            if (!decimal.TryParse(goalSteps, out goalStepCount)) throw new ArgumentException(message: "Goal must be numeric", paramName: nameof(goalSteps));
+            if (!decimal.TryParse(actualSteps, out actualStepCount)) throw new ArgumentException(message: "Actual steps must be numeric", paramName: nameof(actualSteps));
+
+            return CalculatePercentOfGoalSteps(goalStepCount, actualStepCount);
+        }
+
+        public decimal CalculatePercentOfGoalSteps(decimal goalStepCount, decimal actualStepCount)
+        {
+            if (goalStepCount <= 0) throw new ArgumentException("Goal must be greater than 0", "goalSteps");
+            return (actualStepCount / goalStepCount) * 100;
+        }
+
         public void ValidateEmail()
         {
             throw new NotImplementedException();
--- DefensiveCoding.Windows/PedometerForm.cs
+++ DefensiveCoding.Windows/PedometerForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DefensiveCoding.BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,29 +20,10 @@ namespace DefensiveCoding.Windows
 
         private void CalculateButton_Click(object sender, EventArgs e)
         {
-            var result = CalculatePercentOfGoalSteps(this.GoalTextBox.Text, this.StepsTextBox.Text);
+            var customer = new Customer();
+            var result = customer.CalculatePercentOfGoalSteps(this.GoalTextBox.Text, this.StepsTextBox.Text);
 
             PercentResultLabel.Text = "You reached " + decimal.Round(result, 2, MidpointRounding.AwayFromZero) + "% of your goal.";
-        }
-
-        private static decimal CalculatePercentOfGoalSteps(string goalSteps, string actualSteps)
-        {
-            decimal goalStepCount = 0;
-            decimal actualStepCount = 0;
-
-            if (string.IsNullOrWhiteSpace(goalSteps)) throw new ArgumentException(message: "Goal must be entered", paramName: nameof(goalSteps));
-            if (string.IsNullOrWhiteSpace(actualSteps)) throw new ArgumentException(message: "Actual steps count must be entered", paramName: nameof(actualSteps));
-
-            if (!decimal.TryParse(goalSteps, out goalStepCount)) throw new ArgumentException(message: "Goal must be numeric", paramName: nameof(goalSteps));
-            if (!decimal.TryParse(actualSteps, out actualStepCount)) throw new ArgumentException(message: "Actual steps must be numeric", paramName: nameof(actualSteps));
-
-            return CalculatePercentOfGoalSteps(goalStepCount, actualStepCount);
-        }
-
-        private static decimal CalculatePercentOfGoalSteps(decimal goalStepCount, decimal actualStepCount)
-        {
-            if (goalStepCount <= 0) throw new ArgumentException("Goal must be greater than 0", "goalSteps");
-            return (actualStepCount / goalStepCount) * 100;
         }
     }
 }
